0d644904acdc010d8af469a94203fb90b4e2e176
Antônio Carlos <85313968+Degocardoso@users.noreply.github.com>
2026-05-07 19:18:06 -0300

    Change Supabase URL in config.js
    Updated Supabase URL to remove the '/rest/v1/' endpoint.

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 // Pegue em: Supabase → Settings → API
 // =====================================================
 
-const SUPABASE_URL = 'https://motjuojjsyzdydydwttx.supabase.co/rest/v1/';
+const SUPABASE_URL = 'https://motjuojjsyzdydydwttx.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1vdGp1b2pqc3l6ZHlkeWR3dHR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxNzkxNzIsImV4cCI6MjA5Mzc1NTE3Mn0.yS9IabesS-dkD6AKq4khLd9Nxd87iNsa1yWou_syyb0';
 
 // =====================================================
